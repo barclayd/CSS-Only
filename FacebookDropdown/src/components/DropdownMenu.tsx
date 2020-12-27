b2f6e7891e1a156d90522058456b3d5a73c49c2f
Daniel Barclay <barclaysd@me.com>
2020-12-27 15:56:19 +0000

    Additional svg icons and more sub menus for Dropdown

--- FacebookDropdown/src/components/DropdownMenu.tsx
+++ FacebookDropdown/src/components/DropdownMenu.tsx
@@ -3,12 +3,18 @@ import { DropdownMenuItem } from './DropdownMenuItem';
 import { ReactComponent as CogIcon } from '../icons/cog.svg';
 import { ReactComponent as ChevronIcon } from '../icons/chevron.svg';
 import { ReactComponent as ArrowIcon } from '../icons/arrow.svg';
-import { CSSTransition } from 'react-transition-group';
+import { ReactComponent as MoonIcon } from '../icons/moon.svg';
+import { ReactComponent as LockIcon } from '../icons/lock.svg';
+import { ReactComponent as LogoutIcon } from '../icons/logout.svg';
+import { ReactComponent as HistoryIcon } from '../icons/history.svg';
+import { ReactComponent as NewsIcon } from '../icons/news.svg';
+import { ReactComponent as GlobeIcon } from '../icons/globe.svg';
+import { Transition } from './Transition';
 
 export enum DropdownMenuOption {
   main = 'main',
   settings = 'settings',
-  help = 'help',
+  theme = 'theme',
 }
 
 export const DropdownMenu: FC = () => {
@@ -17,49 +23,47 @@ export const DropdownMenu: FC = () => {
   );
 
   const onMenuItemClick = (menu: DropdownMenuOption) => {
-    console.log('called');
     setActiveMenu(menu);
-    console.log(activeMenu);
   };
 
   return (
     <div className="dropdown">
-      <CSSTransition
-        in={activeMenu === DropdownMenuOption.main}
-        unmountOnExit
-        timeout={500}
-        classNames="menu-primary"
+      <Transition
+        active={activeMenu === DropdownMenuOption.main}
+        className="menu-primary"
       >
-        <div className="menu">
-          <DropdownMenuItem
-            onClick={() => onMenuItemClick(DropdownMenuOption.help)}
-          >
-            My Profile
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            leftIcon={CogIcon}
-            rightIcon={ChevronIcon}
-            onClick={() => onMenuItemClick(DropdownMenuOption.settings)}
-          >
-            Settings
-          </DropdownMenuItem>
-        </div>
-      </CSSTransition>
+        <DropdownMenuItem
+          leftIcon={MoonIcon}
+          rightIcon={ChevronIcon}
+          onClick={() => onMenuItemClick(DropdownMenuOption.theme)}
+        >
+          Theme
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          leftIcon={CogIcon}
+          rightIcon={ChevronIcon}
+          onClick={() => onMenuItemClick(DropdownMenuOption.settings)}
+        >
+          Settings
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          leftIcon={LogoutIcon}
+          onClick={() => onMenuItemClick(DropdownMenuOption.settings)}
+        >
+          Logout
+        </DropdownMenuItem>
+      </Transition>
 
-      <CSSTransition
-        in={activeMenu === DropdownMenuOption.settings}
-        unmountOnExit
-        timeout={500}
-        classNames="menu-secondary"
-      >
-        <div className="menu">
-          <DropdownMenuItem
-            leftIcon={ArrowIcon}
-            onClick={() => onMenuItemClick(DropdownMenuOption.main)}
-          />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-        </div>
-      </CSSTransition>
+      <Transition active={activeMenu === DropdownMenuOption.settings}>
+        <DropdownMenuItem
+          leftIcon={ArrowIcon}
+          onClick={() => onMenuItemClick(DropdownMenuOption.main)}
+        />
+        <DropdownMenuItem leftIcon={LockIcon}>Privacy</DropdownMenuItem>
+        <DropdownMenuItem leftIcon={HistoryIcon}>Activity</DropdownMenuItem>
+        <DropdownMenuItem leftIcon={NewsIcon}>News Feed</DropdownMenuItem>
+        <DropdownMenuItem leftIcon={GlobeIcon}>Language</DropdownMenuItem>
+      </Transition>
     </div>
   );
 };
