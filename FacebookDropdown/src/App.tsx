9f6b0770f196ec83352f8e4ccb29f4ca44615f3c
Daniel Barclay <barclaysd@me.com>
2020-12-27 15:23:34 +0000

    Tidy up CSS and add react-transition-group

--- FacebookDropdown/src/App.tsx
+++ FacebookDropdown/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { NavBar } from './components/NavBar';
 import { NavBarItem } from './components/NavBarItem';
 import { ReactComponent as PlusIcon } from './icons/plus.svg';
