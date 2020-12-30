047c1026db2ebc962faa8e2ccd197d381b56d791
Daniel Barclay <barclaysd@me.com>
2020-12-30 12:18:03 +0000

    Control translateZ of other items on page, add transition to smooth out animation

--- 3DProductCard/index.js
+++ 3DProductCard/index.js
@@ -1,5 +1,10 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
+const title = document.querySelector('.title');
+const shoe = document.querySelector('.shoe img');
+const purchase = document.querySelector('.purchase');
+const description = document.querySelector('.description');
+const sizes = document.querySelector('.sizes');
 
 const ANIMATION_EASE = 25;
 
@@ -11,9 +16,21 @@ container.addEventListener('mousemove', (event) => {
 
 container.addEventListener('mouseenter', () => {
   card.style.transition = 'none';
+  title.style.transform = 'translateZ(150px)'
+  shoe.style.transform = 'translateZ(200px) rotateZ(35deg)'
+  description.style.transform = 'translateZ(75px)'
+  description.style['text-shadow'] = '0 5px 14px rgba(0, 0, 0, 0.28)'
+  sizes.style.transform = 'translateZ(100px)'
+  purchase.style.transform = 'translateZ(75px)'
 });
 
 container.addEventListener('mouseleave', () => {
   card.style.transition = 'all 1000ms ease';
   card.style.transform = `rotateY(0) rotateX(0)`;
+  title.style.transform = 'translateZ(0)'
+  shoe.style.transform = 'translateZ(0) rotateZ(0)'
+  description.style.transform = 'translateZ(0)'
+  sizes.style.transform = 'translateZ(0)'
+  purchase.style.transform = 'translateZ(0)'
+  description.style['text-shadow'] = 'none'
 });
