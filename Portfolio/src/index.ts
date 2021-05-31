239528173e51986e31f041114f525445eef800d6
Daniel Barclay <barclaysd@me.com>
2021-05-31 22:57:30 +0100

    Add controls for torus

--- Portfolio/src/index.ts
+++ Portfolio/src/index.ts
@@ -1,6 +1,7 @@
 import './style.css';
 import * as THREE from 'three';
 import torus from './torus';
+import { orbitControls } from './orbitControls';
 
 const ASPECT_RATIO = window.innerWidth / window.innerHeight;
 const INITAL_Z_POSITION = 30;
@@ -21,9 +22,18 @@ camera.position.setZ(INITAL_Z_POSITION);
 
 scene.add(torus.shape);
 
+scene.add(torus.pointLight, torus.ambientLight);
+
+scene.add(torus.lightHelper);
+
+scene.add(torus.gridHelper);
+
+const controls = orbitControls(camera, renderer.domElement);
+
 const animate = () => {
   requestAnimationFrame(animate);
   torus.rotate();
+  controls.update();
   renderer.render(scene, camera);
 };
 
