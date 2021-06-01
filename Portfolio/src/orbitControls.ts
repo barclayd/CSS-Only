92f810d4b72eaf2e0790cff7f1b8cf513c28c150
Daniel Barclay <barclaysd@me.com>
2021-06-01 22:35:21 +0100

    moveCamera method, update moon position

--- Portfolio/src/orbitControls.ts
+++ Portfolio/src/orbitControls.ts
@@ -4,4 +4,9 @@ import type { PerspectiveCamera } from 'three';
 const orbitControls = (camera: PerspectiveCamera, dom: HTMLCanvasElement) =>
   new OrbitControls(camera, dom);
 
-export { orbitControls };
+const moveCamera = (animateTextures: (top: number) => void) => {
+  const top = document.body.getBoundingClientRect().top;
+  animateTextures(top);
+};
+
+export { orbitControls, moveCamera };
